a6e4210cf0ba562bb61041d8957844fe45070d84
sxdny <67187745+sxdny@users.noreply.github.com>
2024-11-27 19:46:14 +0100

    feat: react-router installed

--- 00-react-blog/src/main.jsx
+++ 00-react-blog/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import CreatePost from './components/CreatePosts.jsx';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<App />}/>
+    <Route path='/create-post' element={<CreatePost />} />
+  </Routes>
+  </BrowserRouter>,
 )
