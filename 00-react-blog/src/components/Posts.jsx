97b38539762631e579d6d5cf022a4611ddf9e332
sxdny <67187745+sxdny@users.noreply.github.com>
2024-11-28 18:57:11 +0100

    feat: posts now in localStorage

--- 00-react-blog/src/components/Posts.jsx
+++ 00-react-blog/src/components/Posts.jsx
@@ -1,19 +1,32 @@
 import './css/Posts.css'
+import { useEffect, useState } from 'react';
 
 export default function Posts() {
 
-  let posts = localStorage.getItem('posts');
-  
-  if (posts.length != 0) {
-    posts = localStorage.getItem('posts')
-  } else {
-    localStorage.setItem('posts', [])
-  }
+  const [posts, setPosts] = useState(() => {
+    const savedPosts = localStorage.getItem('posts');
+    return savedPosts ? JSON.parse(savedPosts) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('posts', JSON.stringify(posts));
+  }, [posts]);
 
   return (
     <main>
       <h1>List of posts</h1>
-      
+      {
+        posts.map((post) => {
+          return (
+            <div key={post.title}>
+              <h1> {post.title} </h1>
+              <p> {post.author} </p>
+              <p> {post.content} </p>
+            </div>
+          )
+          
+        })
+      }
     </main>
   );
 }
